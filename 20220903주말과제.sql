e2e95c4e83ccd3497a4b603658fd41fcddc1ea22
Myodong <gyehd1230@naver.com>
2022-09-13 17:53:52 +0900

    WORKBOOK
    3번 풀이중

--- 20220903주말과제.sql
+++ 20220903주말과제.sql
@@ -243,7 +243,53 @@ ORDER BY SUBSTR(TERM_NO,1,4),SUBSTR(TERM_NO,5,2);
 
 
 
-  
+-----------------------------------------------------------------------------------  
+/*
+ 1. 학생이름과 주소지를 표시하시오. 
+ 단, 출력헤더는 "학생이름","주소지"로 하고 정렬은 이름으로 오름차순 표시하도록 한다.*/
+
+SELECT STUDENT_NAME 학생이름,STUDENT_ADDRESS 주소지
+FROM TB_STUDENT
+ORDER BY STUDENT_NAME ASC;
+
+
+
+/*
+ 2. 휴학중인 학생들의 이름과 주민번호를 나이가 적은 순서로 화면에 출력하시오 */
+
+SELECT STUDENT_NAME, STUDENT_SSN
+FROM TB_STUDENT
+WHERE ABSENCE_YN = 'Y'
+ORDER BY STUDENT_SSN DESC;
+
+
+/*
+3. 주소지가 강원도나 경기도인 학생들 중 1900년대 학번을 가진 학생들의
+이름과 학번, 주소를 이름의 오름차순으로 화면에 출력하시오.
+단, 출력헤더에는 "학생이름", "학번", "거주지 주소"가 출력되도록 한다.*/
+
+
+SELECT STUDENT_NAME 학생이름, STUDENT_NO 학번, STUDENT_ADDRESS "거주지 주소"
+FROM TB_STUDENT
+WHERE ENTRANCE_DATE LIKE '9%'
+AND STUDENT_ADDRESS LIKE '%경기도%'
+OR STUDENT_ADDRESS LIKE '%강원도%'
+ORDER BY STUDENT_NAME ASC;
+
+
+/*
+ 4. 현재 법학과 교수 중 가장 나이가 많은 사람부터 이름을 확인할 수 있는 SQL 문장을 작성하시오.
+(법학과의 '학과 코드'는 학과 테이블을 조회해서 찾아 내도록 하자)  JOIN으로 풀어도 됩니다!
+ */
+
+SELECT PROFESSOR_NAME, PROFESSOR_SSN,DEPARTMENT_NO --CLASS_NO,CLASS_NAME
+FROM TB_PROFESSOR 
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO);
+WHERE CLASS_NAME= '법학과'
+
+
+
+
 
 
 
